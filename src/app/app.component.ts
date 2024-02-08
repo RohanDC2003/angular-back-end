b06850ae478851153890c0f4e09311580b712b20
RohanDC2003 <rohandc2003@gmail.com>
2024-02-08 16:16:56 +0530

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './api.service';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SlicePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
